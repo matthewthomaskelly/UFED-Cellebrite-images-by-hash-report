58b54118f31a2316da9cfa98f9ab5853ca5e92c5
Matthew Kelly <Matthew@springfield.local>
2016-11-04 15:47:46 +0000

    1.5

--- 1.2 UFED tag images by hash.py
+++ 1.2 UFED tag images by hash.py
@@ -1,12 +1,13 @@
 # *******************************************************************
 # ** Name:          UFED tag images by hash
-# ** Version:       v1.4
+# ** Version:       v1.5
 # ** Purpose:       A short script to open exported CSV separated export from NetClean of categorised images including MD5 value.
 #					The script will iterate through each image file witin an extraction and tag those images located.
 #    11/05/2016      - Amended purpose to include writing HTML report.
 #    20/05/2016		 - Amended coding to include PIL and to produce thumbs rather than original images in report 
 #    30/08/2016      - 1.3 - Changed reference to PIL to CLR Image as UFED Python uses IronPython 2.6 and PIL not supported.
 #    21/09/2016      - 1.4 - Form function to select report location and CSV file location
+#    +01/10/2016     - 1.5 - Testing phase before release!
 # ** Returns:       None - file located and not-located or duplicates will be logged.
 # ** Variables:     N/A
 # ** Author:        Matthew KELLY
@@ -48,12 +49,19 @@ def main():
     # Get location of CSV file and path for log file
     # 06/05/2016 hard coded at this point
     #sCSVFileLoc = strSelectFile()
-    #sExportReportLoc = strSelectFolder()
-    frmMain = IForm()
-    Application.Run(frmMain)
-    sCSVFileLoc = frmMain.FolderPath
+    frmCSVFolder = IForm()
+    frmCSVFolder.AddHeading("Please enter full pathname location for CSV values")
+    frmCSVFolder.AddPathTitle( os.getcwd() )
+    Application.Run( frmCSVFolder )
+    sCSVFileLoc = frmCSVFolder.pathName
 
     #sExportReportLoc = strSelectFolder()
+    frmReportFolder = IForm()
+    frmReportFolder.AddHeading("Please select location for Report")
+    frmReportFolder.AddPathTitle( os.getcwd() )
+    Application.Run( frmReportFolder )
+    sExportReportLoc = frmReportFolder.pathName
+
     sImagesRelLoc = '\\Images'
     sThumbRelLoc = '\\Thumbs'
     sReportName = 'report.html'
@@ -84,7 +92,7 @@ def main():
                 if strMD5 == '':
                     strMD5 = getMd5HashValue(eachImage)
                 if strMD5  == eachLineSplit[iHASHIndex].strip():
-                    print( 'Match!' + strMD5 )
+                    print( 'Match! ' + strMD5 )
                     # save image to images location
                     try:
                         exportUFEDFile(eachImage, sExportReportLoc + sImagesRelLoc)
@@ -100,18 +108,18 @@ def main():
                     print (sExportReportLoc + sThumbRelLoc + '\\' + eachImage.Name)
                     
                     try:
-                        objThumbImage.Save(sExportReportLoc + sThumbRelLoc + '\\' + eachImage.Name)
+                        objThumbImage.Save(sExportReportLoc + sThumbRelLoc + '\\' + eachImage.Name + '.png')
                     except:
                          print('error creating thumbnail')
                          
                     objThumbImage.Dispose
                     objImageFile.Dispose
 
-                    sThumbName = eachImage.Name
+                    sThumbName = eachImage.Name + '.png'
                     # add relative path to HTML
-                    objHTMLWrite.AddImageLocationReference(eachLineSplit[iCategoryIndex], sImagesRelLoc + '\\' + sThumbName, sImagesRelLoc + '\\' + eachImage.Name)
+                    objHTMLWrite.AddImageLocationReference(eachLineSplit[iCategoryIndex], sThumbRelLoc + '\\' + sThumbName, sImagesRelLoc  + '\\' + eachImage.Name)
                     # add file information to table content
-                    lstDetails = [eachImage.Name, eachImage.Md5]
+                    lstDetails = ['Name: ' + eachImage.Name, 'Path@ ' + eachImage.Folder, 'Creation date: ' + eachImage.CreationTime, 'MD5: ' + eachImage.Md5 ]
                     objHTMLWrite.AddTableContentByKeyAsLists( eachLineSplit[iCategoryIndex], lstDetails )
 
         print( str(iLineCount) + ' ' + eachLineSplit[iHASHIndex] )
@@ -125,6 +133,13 @@ def main():
     print(sExportReportLoc + '\\' +  sReportName)
     objHTMLWrite.WriteHTMLtoFile(sExportReportLoc + '\\' + sReportName)
 
+# *******************************************************************
+# ** Name:          resizeImage
+# ** Purpose:       
+# ** Author:        Matthew KELLY
+# ** Date:          
+# ** Revisions:     
+# ****************************************************************** 
 def resizeImage (r_objImage):
     xTo, yTo = 100, 100
     xNow, yNow = r_objImage.Width, r_objImage.Height
@@ -144,6 +159,13 @@ def resizeImage (r_objImage):
 def ThumbnailCallBack():
     return False
 
+# *******************************************************************
+# ** Name:          getMd5HashValue
+# ** Purpose:       
+# ** Author:       
+# ** Date:          
+# ** Revisions:     
+# ****************************************************************** 
 def getMd5HashValue (r_objImageFile):
 
     #print (objImageFile.ToString())
@@ -155,24 +177,85 @@ def getMd5HashValue (r_objImageFile):
         return hexMD5.upper
     except:
         return ''
-    
+
+# *******************************************************************
+# ** Name:          IForm
+# ** Purpose:       Takes an Image from ds in UFED Cellebrite and exports to file-path specified
+# ** Author:        Unknown - MET Police
+# ** Date:          
+# ** Revisions:     06/05/2016 - removed hash library reference
+# ****************************************************************** 
 def strSelectFolder():
     dialog = FolderBrowserDialog()
     if ( dialog.ShowDialog() == DialogResult.OK):
         return dialog.SelectedPath
 
+# *******************************************************************
+# ** Name:          IForm
+# ** Purpose:       Takes an Image from ds in UFED Cellebrite and exports to file-path specified
+# ** Author:        Unknown - MET Police
+# ** Date:          
+# ** Revisions:     06/05/2016 - removed hash library reference
+# ****************************************************************** 
 def strSelectFile():
     dialog = OpenFileDialog()
     dialog.Filter = "CSV files (*.csv)|*.csv"
     if ( dialog.ShowDialog() == DialogResult.OK ):
         return dialog.FileName
 
+# *******************************************************************
+# ** Name:          IForm
+# ** Purpose:       Takes an Image from ds in UFED Cellebrite and exports to file-path specified
+# ** Author:        Unknown - MET Police
+# ** Date:          
+# ** Revisions:     06/05/2016 - removed hash library reference
+# ****************************************************************** 
 class IForm(Form):
+
         def __init__(self):
-                dialog = FolderBrowserDialog()
-                if ( dialog.ShowDialog() == DialogResult.OK):
-                    self.FolderPath = dialog.SelectedPath
-                pass
+                self.Text = "Select Path"
+                self.Height = 100
+
+                #add button
+                self.button1 = Button()
+                self.button1.Text = "&OK"
+                self.button1.Location = Point(10, 35)
+                self.button1.Click += self.OKPressed
+
+                #add textbox
+                self.textbox = TextBox()
+                self.textbox.Text = "Path goes here"
+                self.textbox.Location = Point(10,10)
+                self.textbox.Width = 250
+
+                self.Controls.Add(self.textbox)
+                self.Controls.Add(self.button1)
+                self.CenterToScreen()
+
+                self.pathName = ""
+
+        def AddHeading(self, v_sHeading):
+                self.Text = v_sHeading
+
+        def AddPathTitle(self, v_sPathTitle):
+                self.textbox.Text = v_sPathTitle        
+
+        def OKPressed(self, sender, args):
+                s = self.textbox.Text
+                if ( os.path.isdir(s) == True ) and ( os.path.isfile(s) == False ):
+                        MessageBox.Show( "This directory is already populated, please choose another." , "Invalid Directory" )
+                else:
+                        r = MessageBox.Show( "This path does not exist. Would you like to create it?", "Invalid Directory", MessageBoxButtons.YesNo, MessageBoxIcon.Question )
+                        if r == DialogResult.Yes:
+                                os.mkdir(s)
+                                os.mkdir(s + "\Images")
+                                os.mkdir(s + "\Thumbs")
+                                self.pathName = s
+                                self.Close()
+                        else:
+                            self.pathName = s
+                            self.Close()
+                            pass  
 
 
 # *******************************************************************
@@ -183,32 +266,35 @@ class IForm(Form):
 # ** Revisions:     06/05/2016 - removed hash library reference
 # ****************************************************************** 
 def exportUFEDFile(pic,path):
-        print(path)
-        fileDataReadsize = 2**25
-        fileSize = pic.Size
-        if (fileSize > 2113929216):
-                MessageBox.Show("%s is greater than 2GB, please review manually. Filename stored in trace window" % (pic.Name),"Error")
-                print ("File %s is over 2gb in size, review manually" % (pic.Name))
-                return "", ""
-        # mtk 06/05/2016 
-        # m = hashlib.md5()
-        filename = pic.Name 
-        filePath = os.path.join(path,filename)
-        ext = os.path.splitext(pic.Name)[1]
-        locateInvalidChar = ext.find("?")
-        if (locateInvalidChar != -1):
-                ext = ext[:locateInvalidChar]
-        print(filePath)
+    print(path)
+    fileDataReadsize = 2**25
+    fileSize = pic.Size
+    if (fileSize > 2113929216):
+        MessageBox.Show("%s is greater than 2GB, please review manually. Filename stored in trace window" % (pic.Name),"Error")
+        print ("File %s is over 2gb in size, review manually" % (pic.Name))
+        return "", ""
+    # mtk 06/05/2016 
+    # m = hashlib.md5()
+    filename = pic.Name 
+    filePath = os.path.join(path,filename)
+    ext = os.path.splitext(pic.Name)[1]
+    locateInvalidChar = ext.find("?")
+    if (locateInvalidChar != -1):
+        ext = ext[:locateInvalidChar]
+    print(filePath) 
+    try:
         f = open(filePath,'wb')
         pic.seek(0)
         filedata = pic.read(fileDataReadsize)
         while len(filedata) > 0:
-                # mtk 06/05/2016
-                # m.update(filedata)
-                f.write(filedata)
-                filedata = pic.read(fileDataReadsize)
+        # mtk 06/05/2016
+        # m.update(filedata)
+            f.write(filedata)
+            filedata = pic.read(fileDataReadsize)
         pic.seek(0)
         f.close()
+    except:
+        return ""
 
 
 
@@ -239,10 +325,12 @@ class clsHTMLWriter:
         self.__AddToDicCategories(v_sKey, sHTMLBuiltString)
      
     def AddImageLocationReference(self, v_sKey, v_sThumbRelativeLocation, v_sImageRelativeLocation = ''):
-        sHTMLBuiltString = '<TR><TD><A href=.' + v_sImageRelativeLocation
+        sHTMLBuiltString = '<TR><TD>'
         if v_sImageRelativeLocation != '':
-            sHTMLBuiltString += '<A href=.' + v_sImageRelativeLocation
-        sHTMLBuiltString += '<IMAGE src=.' + v_sThumbRelativeLocation + '>'
+            sHTMLBuiltString += '<A href=.' + v_sImageRelativeLocation + '>'
+        else:
+            sHTMLBuiltString += '<A href=.' + v_sThumbRelativeLocation + '>', 
+        sHTMLBuiltString += '<IMG src=.' + v_sThumbRelativeLocation + '>'
         sHTMLBuiltString += '</A></TD></TR>'
         self.__AddToDicCategories(v_sKey, sHTMLBuiltString)
     
@@ -267,13 +355,13 @@ class clsHTMLWriter:
             print(sTableContent)
             sTableContent = '<TD>' + str(sTableContent) + '</TD>'
             sHTMLBuiltString += str(sTableContent)
-            sHTMLBuiltString += '</TR>'
+            sHTMLBuiltString += '</TR>'  
         return (sHTMLBuiltString)
 
     def __AddToDicCategories(self, v_sKey, v_sHTMLBuiltString):
         if v_sKey in self.__dicCategories:
             self.__dicCategories[v_sKey] += v_sHTMLBuiltString
-        else:
+        else:  
             self.__dicCategories[v_sKey] = v_sHTMLBuiltString
 
 
